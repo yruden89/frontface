31bed190480152b700f91c618149c8fd1c27e7bb
Eugene Rudenko <yruden@softserveinc.com>
2014-10-16 18:03:26 +0400

    infrastructure

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,52 @@
-var DEV_SERVER_PORT = 8080,
-    LIVERELOAD_SERVER_PORT = 35729,
-
+var DEV_SERVER_PORT = 8080
 
     gulp = require("gulp"),
     express = require("express"),
-    lr = require("tiny-lr")(),
+    livereload = require("gulp-livereload"),
     path = require('path'),
+    jade = require("gulp-jade"),
+    clean = require("gulp-clean"),
+    runSequence = require("run-sequence"),
     devServer = express();
 
+gulp.task("cleanViews", function(){
+    return gulp.src("./build/views", {read: false})
+        .pipe(clean());
+});
+
+gulp.task("compileViews", ["cleanViews"], function(){
+    return gulp.src("./dev/views/*.jade")
+        .pipe(jade({pretty: true}))
+        .pipe(gulp.dest("./build/views/"));
+});
+
+gulp.task("watchViews", function(next){
+    var changed = livereload();
+    gulp.watch("./dev/views/*.jade", function(event){
+        gulp.src(event.path)
+            .pipe(jade({pretty: true}))
+            .pipe(gulp.dest("./build/views"))
+            .pipe(livereload())
+    });
+    next();
+});
+
 function startupDevServer(){
-    devServer.use(express.static(__dirname + "/dev"));
+    devServer.use(express.static(__dirname + "/build"));
     devServer.listen(DEV_SERVER_PORT, function(){
         console.log("debugging server is up ");
     });
-    lr.listen(LIVERELOAD_SERVER_PORT);
-    gulp.watch("./dev/*.html", function(event){
-        var fileName = path.relative(__dirname, event.path);
-
-        lr.changed({
-            body: {
-                files: [fileName]
-            }
-        });
-    });
 }
 
-gulp.task("buildDev", function(){
-    startupDevServer();
+gulp.task("buildDev", function(next){
+    runSequence(
+        "compileViews",
+        "watchViews",
+        function(){
+            startupDevServer();
+            next();
+        }
+    );
 });
 
 gulp.task("buildProd", function(){
